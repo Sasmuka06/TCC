29e554ca98632dcbe447191148baf55f6b6b72e3
Sasmuka06 <samuelssf881@gmail.com>
2025-06-11 17:02:54 -0300

    nâo lembro o que fiz, possivelmente é só um teste

--- usuario.php
+++ usuario.php
@@ -20,7 +20,7 @@
     </div>
 
     <!-- Conteúdo principal -->
-    <main class="conteudo">
+    <div class="conteudo">
         <!-- Foto do usuário -->
         <div class="foto-usuario">
             <img src="image_teste1.jpg" alt="Foto do usuário">
@@ -37,7 +37,7 @@
 
         <h1>Bem-vindo!</h1>
         <p>Lorem ipsum dolor sit amet. Qui quaerat praesentium et autem iure non quia velit vel voluptatem magni qui quia voluptate sed sunt voluptatem id dolorem enim. Et provident assumenda vel atque soluta ut voluptatem excepturi qui unde possimus ut placeat quia. Ex fuga sunt qui impedit adipisci vel blanditiis aliquid id cupiditate unde a perspiciatis expedita. Est autem nemo et nisi repellendus est voluptas praesentium et ipsam accusantium sit voluptatum fugit. Est error velit aut numquam inventore ut inventore mollitia et consequuntur architecto vel quae beatae eos iure commodi. Non magni eius rem minima enim et corrupti officiis aut asperiores praesentium. Eum omnis quia quo quos autem ut neque iure nam excepturi quos et perferendis illum ab quaerat laboriosam. Eum dolore vitae est sint voluptatem quo cupiditate rerum.</p>
-    </main>
+</div>
 </div>
 </body>
 
