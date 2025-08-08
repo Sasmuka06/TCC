1640cfabca2e5d500d5e0a1e96b3e910256a4ed2
Sasmuka06 <samuelssf881@gmail.com>
2025-08-08 17:55:36 -0300

    Retirei uum arquivo que era praticamente inutil, mudei o nome de um arquivo de usuario.php para sindico.php, fiz uma sidebar temporaria, para futuramente aprimora-la

---
+++ sindico.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="corpo.css">
+    <title>Sindico</title>
+</head>
+    <body>
+<div class="container-geral">
+    <!-- Barra lateral -->
+<?php include 'sidebar.php'; ?>
+
+</div>
+
+    <!-- Conteúdo principal -->
+    <div class="conteudo">
+        <!-- Foto do usuário -->
+        <div class="foto-usuario">
+            <img src="image_teste1.jpg" alt="Foto do usuário">
+            <p class="nome-usuario">Nome do Usuário</p>
+</div>
+</body>
+</html>
